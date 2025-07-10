e16586e6388811a4a66895a789ec2cc970e51d9a
muhammadrifaii <rifairafai77@gmail.com>
2025-07-10 21:35:14 +0700

    menambah fitur baru2

--- src/views/laporan.vue
+++ src/views/laporan.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="laporan">
     <h1>Laporan Keuangan</h1>
-    <p>Laporan bulanan dan tahunan tersedia di sini.</p>
+
+    <h3>Total Transaksi: Rp {{ totalTransaksi }}</h3>
+    <h3>Total Pengeluaran: Rp {{ totalPengeluaran }}</h3>
+    <h3>Keuntungan Bersih: Rp {{ totalTransaksi - totalPengeluaran }}</h3>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Laporan',
+  data() {
+    return {
+      transaksi: [],
+      pengeluaran: []
+    };
+  },
+  computed: {
+    totalTransaksi() {
+      return this.transaksi.reduce((acc, t) => acc + this.parseRupiah(t.total), 0);
+    },
+    totalPengeluaran() {
+      return this.pengeluaran.reduce((acc, p) => acc + Number(p.jumlah), 0);
+    }
+  },
+  methods: {
+    parseRupiah(rp) {
+      return Number(rp.replace(/[^\d]/g, ''));
+    },
+    async getData() {
+      const trx = await axios.get('http://localhost:3000/transaksi');
+      const peng = await axios.get('http://localhost:3000/pengeluaran');
+      this.transaksi = trx.data;
+      this.pengeluaran = peng.data;
+    }
+  },
+  mounted() {
+    this.getData();
+  }
 };
 </script>
 
@@ -15,5 +48,8 @@ export default {
 .laporan {
   padding: 20px;
   background-color: #ecfdf5;
+}
+h3 {
+  margin: 8px 0;
 }
 </style>
